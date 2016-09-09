53892202292d760b0e7e66abcbd35161b12416e9
Christopher CHEN <chenshaoming@appgame.com>
2016-09-09 22:06:35 +0800

    refactor: move demo routes here from phwoolcon/bootstrap

---
+++ phwoolcon-package/phwoolcon-package-demo.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'phwoolcon/demo' => [
+        'config' => 'demo.php',
+        'assets' => [
+            'phwoolcon-auth/sso.js',
+            'phwoolcon-auth/sso.min.js',
+        ],
+        'routes' => [
+            100 => 'routes.php',
+        ],
+    ],
+];
